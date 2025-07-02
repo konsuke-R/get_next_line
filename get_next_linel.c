9cc8ad1a86347dcd29da82aa57836bceadb53f72
konsuke <zootutoro@gmail.com>
2025-07-03 07:45:10 +0900

    psh

--- get_next_linel.c
+++ get_next_linel.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 21:40:45 by user              #+#    #+#             */
-/*   Updated: 2025/06/28 15:00:29 by user             ###   ########.fr       */
+/*   Updated: 2025/06/30 23:50:37 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ char *get_next_line(int fd) {
 
     read_byte = 0;
 
-    if (fd < 0|| BUFER_SIZE <= 0) {
+    if (fd < 0|| BUFFER_SIZE <= 0) {
         return (0);
     }
 
-    buf = til_n_str(sava_line);
+    buf = til_n_str(save_line);
     save_line = after_n_str(save_line);
     if (save_line == NULL) {
-        read_byte = read(fd, save_line, BUFER_SIZE);
+        read_byte = read(fd, save_line, BUFFER_SIZE);
     }
 
     if (read_byte <= 0) {
@@ -39,28 +39,30 @@ char *get_next_line(int fd) {
     return buf;
 }
 
-char *til_n_str(*buf) {
+char *til_n_str(char *buf) {
     int i;
-    char *cp_buf;
     i = 0;
+
+    if (!buf)
+        return NULL;
     // 改行文字以外をコピー
-    while (buf[i] && buf[i] != '\n') {
-        cp_buf[i] = buf[i];
+    while (buf[i] && buf[i] != '\n'){
         i++;
     }
 
     // 改行文字をコピー
     // ex)abcde\n
     if (buf[i] && buf[i] == '\n') {
-        cp_buf[i] = buf[i];
         i++;
     }
 
-    // コピーするものがなかったらnull
-    if (cp_buf == NULL) {
+    char *cp_buf = (char*)malloc(i + 1);
+    if (!cp_buf)
         return NULL;
-    }
 
+    for (int j = 0; j < i; j++) {
+        cp_buf[j] = buf[j];
+    }
     // 終端文字を追加
     cp_buf[i] = '\0';
 
@@ -68,28 +70,25 @@ char *til_n_str(*buf) {
     return cp_buf;
 }
 
-char *after_n_str(*buf) {
+char *after_n_str(char *buf) {
+    if (!buf)
+        return NULL;
     int i;
-    int j;
-    char *cp_save_buf;
     i = 0;
-    j = 0;
 
     // 添え字iを改行文字まで移動
-    while (buf[i] != '\n') {
+    while (buf[i] && buf[i] != '\n') {
         i++;
     }
 
-    if (i == 0) {
+    if (buf[i] == '\0')
         return NULL;
-    }
-
     // 改行文字の次へ
     i++;
 
     // 改行文字以降をコピー
     while (buf[i]) {
-        cp_buf[j++] = buf[i++]
+        cp_buf[j++] = buf[i++];
     }
 
     // 終端文字を追加
